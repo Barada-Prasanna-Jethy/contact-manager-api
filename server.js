07dc2d7d1aed1cc9840e2d613e1605df069b55a7
Barada-Prasanna-Jethy <baradajethy@gmail.com>
2020-05-14 23:20:53 +0530

    test1

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require("http");
-const app = require("./app");
+const app = require("./web");
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log("server started at http://localhost:", port);
 });
-//ds
